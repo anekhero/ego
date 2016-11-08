79d26549fe37c93a3b410a6a5c5e05986cf43f85
buzzer <buzzer@it7.com.ua>
2016-11-08 10:24:27 +0200

    Add draft classes

---
+++ ego/body-controller.ts
@@ -0,0 +1,6 @@
+/**
+ * Created by Buzzer on 03.11.2016
+ */
+
+export class BodyController {
+}
--- ego/creature.ts
+++ ego/creature.ts
@@ -2,11 +2,27 @@
  * Created by Buzzer on 21.10.2016.
  */
 import {EnergyCell} from './energy-cell';
+import {DigestiveSystem} from './digestive-system';
+import {Motivator} from './motivator';
+import {Wishmaster} from './wishmaster';
+import {Musculoskeletal} from './musculoskeletal';
+import {BodyController} from './body-controller';
 
-class Creature {
+export class Creature {
     energy: EnergyCell;
+    digestiveSystem: DigestiveSystem;
+    motivator: Motivator;
+    wishmaster: Wishmaster;
+    musculoskeletal: Musculoskeletal;
+    bodyController: BodyController;
+
 
     constructor() {
         this.energy = new EnergyCell();
+        this.digestiveSystem = new DigestiveSystem();
+        this.motivator = new Motivator();
+        this.wishmaster = new Wishmaster();
+        this.musculoskeletal = new Musculoskeletal();
+        this.bodyController = new BodyController();
     }
 }
